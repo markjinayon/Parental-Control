526ac3cad5e79392633238e6e2cdbb8e602c726b
m.lesterjinayon <m.lesterjinayon@gmail.com>
2022-05-17 04:09:39 +0800

    added forgot password functionality

--- app/src/main/java/com/parentalcontrol/seesharp/SignInActivity.java
+++ app/src/main/java/com/parentalcontrol/seesharp/SignInActivity.java
@@ -13,12 +13,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class SignInActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
-    private TextView textViewSignUp_signIn;
+    private TextView textViewSignUp_signIn, textViewForgotPassword_signIn;
     private Button buttonSignIn_signIn;
     private EditText editTextEmail_signIn, editTextPassword_signIn;
     private ProgressBar progressBar_signIn;
@@ -41,10 +43,13 @@ public class SignInActivity extends AppCompatActivity {
         this.textViewSignUp_signIn = (TextView) findViewById(R.id.textViewSignup_signIn);
         this.textViewSignUp_signIn.setOnClickListener(view -> openSignUpActivity());
 
-        // assign button
+        // assign action to sign in
         this.buttonSignIn_signIn = (Button) findViewById(R.id.buttonSignIn_signIn);
         this.buttonSignIn_signIn.setOnClickListener(view -> signIn());
 
+        // assign action to forgot password
+        this.textViewForgotPassword_signIn = (TextView) findViewById(R.id.textViewForgotPassword_signIn);
+        this.textViewForgotPassword_signIn.setOnClickListener(view -> forgotPassword());
     }
 
     public void signIn() {
@@ -74,6 +79,34 @@ public class SignInActivity extends AppCompatActivity {
                     this.progressBar_signIn.setVisibility(View.GONE);
                 })
                 .addOnFailureListener(e -> Log.e("FirebaseAuth Failure", e.toString()));
+    }
+
+    public void forgotPassword() {
+        String email = this.editTextEmail_signIn.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            this.editTextEmail_signIn.setError("Enter your email!");
+            this.editTextEmail_signIn.requestFocus();
+            return;
+        }
+
+        this.progressBar_signIn.setVisibility(View.VISIBLE);
+        this.mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(SignInActivity.this, "Check your email to reset your password!", Toast.LENGTH_LONG).show();
+                    }
+                    this.progressBar_signIn.setVisibility(View.GONE);
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("ERROR", e.toString());
+                    if (e instanceof FirebaseAuthInvalidCredentialsException || e instanceof FirebaseAuthInvalidUserException) {
+                        this.editTextEmail_signIn.setError("Invalid email!");
+                        this.editTextEmail_signIn.requestFocus();
+                    } else {
+                        Toast.makeText(SignInActivity.this, "Please try again! Something went wrong.", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 
     public void openSignUpActivity() {
